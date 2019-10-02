5fc600e47854637eab9dd71d5c3b27b6f8f39fd9
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-10-03 01:51:40 +0900

    Create 17206(2).py

---
+++ Algorithm/Baekjoon/17206 준석이의 수학 숙제/17206(2).py
@@ -0,0 +1,16 @@
+#-*- encoding: utf-8 -*-
+import sys
+r=sys.stdin.readline
+
+T = int(r())        # 테스트 케이스 개수
+
+result = {0:0}
+
+for i in range(1, 80001):   # 80000까지 3의 배수 혹은 7의 배수의합을 result에 넣는다
+    if i % 3 == 0 or i % 7 == 0:
+        result[i] = result[i-1] + i
+    else:
+        result[i] = result[i-1]
+
+for N in map(int,r().split()):
+    print(result[N])
