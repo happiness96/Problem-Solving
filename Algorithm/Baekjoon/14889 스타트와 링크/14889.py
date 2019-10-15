e1ffdc07df95d1d392a033191736d554d6e9bf96
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-10-15 20:22:45 +0900

    Create 14889.py

---
+++ Algorithm/Baekjoon/14889 스타트와 링크/14889.py
@@ -0,0 +1,33 @@
+#-*- encoding: utf-8 -*-
+import sys
+from itertools import combinations 
+r=sys.stdin.readline
+
+N = int(r())        # 인원 수
+people = [num for num in range(1, N+1)]
+
+S = {}              # 능력치
+result = []
+
+for i in range(1, N+1):     # 능력치 입력
+    S[i] = [0] + list(map(int, r().split()))
+
+for start_team in list(combinations(people, N//2)):         # 완전  탐색(브루트포스)
+    start = 0           # 스타트 팀 점수
+    link = 0            # 링크 팀 점수
+    
+    if start_team[0] != 1: break
+    
+    for i in range(1, N+1):
+        if i in start_team:
+            for j in start_team:
+                start += S[i][j]
+                
+        elif not i in start_team:
+            for j in range(1, N+1):
+                if not j in start_team:
+                    link += S[i][j]
+    
+    result.append(abs(start-link))
+
+print(min(result))
