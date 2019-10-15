a881ca73288a630ec44a87f4749209a4e5f6778a
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-10-15 21:47:44 +0900

    Update 14889.py

--- Algorithm/Baekjoon/14889 스타트와 링크/14889.py
+++ Algorithm/Baekjoon/14889 스타트와 링크/14889.py
@@ -13,8 +13,13 @@ for i in range(1, N+1):     # 능력치 입력
     S[i] = [0] + list(map(int, r().split()))
 
 for start_team in list(combinations(people, N//2)):
+    link_team = []
     start = 0           # 스타트 팀 점수
     link = 0            # 링크 팀 점수
+    
+    for i in range(1, N+1):
+        if not i in start_team:
+            link_team.append(i)
     
     if start_team[0] != 1: break
     
@@ -23,11 +28,11 @@ for start_team in list(combinations(people, N//2)):
             for j in start_team:
                 start += S[i][j]
                 
-        elif not i in start_team:
-            for j in range(1, N+1):
-                if not j in start_team:
-                    link += S[i][j]
-                    
+    for i in range(1, N+1):
+        if i in link_team:
+            for j in link_team:
+                link += S[i][j]
+                
     diff = abs(start-link)
     
     if diff :
