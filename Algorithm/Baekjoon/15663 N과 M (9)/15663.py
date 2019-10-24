6795198a5ef6d616c6aa2172c331c57689c9ab97
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-10-24 17:40:52 +0900

    Create 15663.py

---
+++ Algorithm/Baekjoon/15663 N과 M (9)/15663.py
@@ -0,0 +1,20 @@
+#-*- encoding: utf-8 -*-
+import sys
+from itertools import permutations
+r=sys.stdin.readline
+
+N, M = map(int, r().split())
+
+l = sorted([num for num in map(int,r().split())])
+
+for i in range(N):
+    l[i] = str(l[i])
+
+printed = {}            # 이미 출력된 것들
+for per in permutations(l, M):
+    result = ' '.join(per)
+    
+    if not result in printed:
+        print(result)
+    
+    printed[result] = 0
