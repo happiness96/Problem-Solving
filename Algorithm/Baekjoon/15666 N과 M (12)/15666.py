364821b7192ceb47533030ea7ae7ff3e1826cc29
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-10-24 18:21:36 +0900

    Create 15666.py

---
+++ Algorithm/Baekjoon/15666 N과 M (12)/15666.py
@@ -0,0 +1,27 @@
+#-*- encoding: utf-8 -*-
+import sys 
+r=sys.stdin.readline
+
+N, M = map(int, r().split())        # N x M
+l = sorted([num for num in map(int,r().split())])
+printed = []                # 이미 출력된 것들
+
+
+def N_and_M(node, result):          # DFS 방식
+    result.append(str(l[node]))
+    
+    if len(result) == M:
+        temp = ' '.join(result)
+        if not temp in printed:
+            print(temp)
+        printed.append(temp)
+    
+    else:
+        for i in range(node, N):
+            N_and_M(i, result)
+    
+    result.pop()
+
+
+for i in range(N):
+    N_and_M(i, [])
