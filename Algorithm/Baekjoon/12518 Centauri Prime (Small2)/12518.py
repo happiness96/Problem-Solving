4261ed340be7d12ce49960d94608edf4b5551cc5
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-11-04 14:49:50 +0900

    Create 12518.py

---
+++ Algorithm/Baekjoon/12518 Centauri Prime (Small2)/12518.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+import sys
+r = sys.stdin.readline
+
+T = int(r())        # The number of test case
+
+for i in range(T):
+    country = r().rstrip()          # The name of country
+    rule = ''
+
+    if country[-1] in 'yY':
+        rule = 'nobody'
+
+    elif country[-1] in 'aeiouAEIOU':
+        rule = 'a queen'
+
+    else:
+        rule = 'a king'
+
+    print('Case #' + str(i+1) + ': ' + country + ' is ruled by ' + rule + '.')
