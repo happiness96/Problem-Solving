1ec619f379d9cdac396187c7259ad90c716d34f0
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-11-10 16:18:31 +0900

    Create 1543.py

---
+++ Algorithm/Baekjoon/01543 문서 검색/1543.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+doc = r_input().rstrip()            # 문서의 길이
+find_me = r_input().rstrip()        # 검색하고 싶은 단어
+
+print(doc.count(find_me))
