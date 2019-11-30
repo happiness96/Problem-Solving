8f3afec4219f883141c3dab07d069eadad373dab
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-11-30 11:08:50 +0900

    Update 1543.py

--- Algorithm/Baekjoon/01543 문서 검색/1543.py
+++ Algorithm/Baekjoon/01543 문서 검색/1543.py
@@ -2,7 +2,7 @@
 import sys
 r_input = sys.stdin.readline
 
-doc = r_input().rstrip()            # 문서의 길이
+doc = r_input().rstrip()            # 문서
 find_me = r_input().rstrip()        # 검색하고 싶은 단어
 
 print(doc.count(find_me))
