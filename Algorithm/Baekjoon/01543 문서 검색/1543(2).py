fa3d565c86ef9a71c76fde3460f26020a5b63d9d
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-11-30 11:09:04 +0900

    Update 1543(2).py

--- Algorithm/Baekjoon/01543 문서 검색/1543(2).py
+++ Algorithm/Baekjoon/01543 문서 검색/1543(2).py
@@ -2,7 +2,7 @@
 import sys
 r_input = sys.stdin.readline
 
-doc = r_input().rstrip()            # 문서의 길이
+doc = r_input().rstrip()            # 문서
 find_me = r_input().rstrip()        # 검색하고 싶은 단어
 cnt = 0         # 등장하는 횟수
 ind = 0         # 현재 인덱스
