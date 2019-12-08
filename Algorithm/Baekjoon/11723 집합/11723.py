5fc83f765a3dd481cb0d01fcbbc1800de98ed435
domokim2 <54839510+domokim2@users.noreply.github.com>
2019-12-08 18:59:04 +0900

    Create 11723.py

---
+++ Algorithm/Baekjoon/11723 집합/11723.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+import sys
+r_input = sys.stdin.readline
+
+M = int(r_input())          # 연산의 개수
+sss = set()
+
+for _ in range(M):
+    order = r_input().rstrip()
+
+    if order == 'all':
+        sss = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+
+    elif order == 'empty':
+        sss = set()
+
+    else:
+        o, x = map(str, order.split())
+        x = int(x)
+
+        if o == 'check':
+            print(1 if x in sss else 0)
+
+        elif o == 'add':
+            sss.add(x)
+
+        elif o == 'remove':
+            if x in sss:
+                sss.remove(x)
+
+        else:
+            if x in sss:
+                sss.remove(x)
+            else:
+                sss.add(x)
